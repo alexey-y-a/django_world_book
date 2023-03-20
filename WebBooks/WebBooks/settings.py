35f5b5f8400478601d659e915326ee5803dccdfa
alexey-y-a <lexus2157@mail.ru>
2023-03-20 15:52:43 +0300

    Added files for deployment in heroku 20.03.2023

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Чтение SECRET_KEY из переменной окружения
 
-SECRET_KEY = os.environ['SECRET_KEY']
-# ИЛИ чтение ключа из файла
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-p#+b6+nylci(o&-@!ro-gbs81#g#eu8#9nxsm91ushvgk3@m-h"
@@ -55,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -144,3 +143,17 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Heroku: Обновление конфигурации базы данных из $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Статичные файлы (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# Абсолютный путь к каталогу, в котором collectstatic
+# будет собирать статические файлы для развертывания.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Упрощенная обработка статическая файлов.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
