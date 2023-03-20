7dce9dfe9aae613722d2cbb0e86a49eaff362123
alexey-y-a <lexus2157@mail.ru>
2023-03-20 13:51:01 +0300

    Первая версия приложения перенесена на github

---
+++ WebBooks/catalog/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from .models import Author, Book, Genre, Language, Status, BookInstance
+
+
+# Register your models here.
+
+
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+
+
+# admin.site.register(Author)
+
+# Определения к классу администратор
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name",
+              ("date_of_birth", "date_of_death")]
+
+# Зарегистрируйте класс admin с соответсвующей моделью
+admin.site.register(Author, AuthorAdmin)
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+# Регистрируем классы администратора для книг
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "genre", "language", "display_author")
+    list_filter = ("genre", "author")
+    inlines = [BooksInstanceInline]
+
+
+# Регистрируем классы администратора для экземпляра книги
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+
